37feacd3acd3445f79cb4f2ed56b5459f7493141
MixterVee <mixtervandal@gmail.com>
2026-09-03 12:48:39 -0400

    Set EAVdrop Windows window icon explicitly

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 using EAVdrop.Services;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace EAVdrop;
 
@@ -11,7 +12,18 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .ConfigureFonts(_ => { });
+            .ConfigureFonts(_ => { })
+            .ConfigureLifecycleEvents(events =>
+            {
+#if WINDOWS
+                events.AddWindows(windows => windows.OnWindowCreated(window =>
+                {
+                    var iconPath = Path.Combine(AppContext.BaseDirectory, "appicon.ico");
+                    if (File.Exists(iconPath))
+                        window.AppWindow.SetIcon(iconPath);
+                }));
+#endif
+            });
 
         builder.Services.AddSingleton<SettingsService>();
         builder.Services.AddSingleton<EmbyApiClient>();
