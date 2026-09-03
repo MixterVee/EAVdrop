b67742a78ff469786dfc82814a23de19239b15d3
MixterVee <mixtervandal@gmail.com>
2026-09-03 15:03:11 -0400

    Fix approved app icon build pipeline

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,32 @@ public partial class App : Application
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(new AppShell());
+        var window = new Window(new AppShell());
+
+#if WINDOWS
+        window.HandlerChanged += (_, _) =>
+        {
+            try
+            {
+                if (window.Handler?.PlatformView is Microsoft.UI.Xaml.Window nativeWindow)
+                {
+                    var iconPath = Path.Combine(AppContext.BaseDirectory, "appicon.ico");
+                    if (File.Exists(iconPath))
+                    {
+                        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                        var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd);
+                        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                        appWindow.SetIcon(iconPath);
+                    }
+                }
+            }
+            catch
+            {
+                // The executable still carries appicon.ico via ApplicationIcon.
+            }
+        };
+#endif
+
+        return window;
     }
 }
