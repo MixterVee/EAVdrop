0bebbdeb28b8b07117e994da60be13bd47d41389
MixterVee <mixtervandal@gmail.com>
2026-09-23 13:37:38 -0400

    Remove temporary Android diagnostic handler

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,8 +1,6 @@
 using Android.App;
 using Android.Content;
 using Android.Content.PM;
-using Android.OS;
-using Android.Runtime;
 
 namespace EAVdrop;
 
@@ -17,55 +15,4 @@ namespace EAVdrop;
     Categories = new[] { Intent.CategoryLeanbackLauncher })]
 public class MainActivity : MauiAppCompatActivity
 {
-    private bool _showingFatalError;
-
-    protected override void OnCreate(Bundle? savedInstanceState)
-    {
-        AndroidEnvironment.UnhandledExceptionRaiser += OnUnhandledException;
-
-        try
-        {
-            base.OnCreate(savedInstanceState);
-        }
-        catch (Exception ex)
-        {
-            ShowFatalError(ex);
-        }
-    }
-
-    protected override void OnDestroy()
-    {
-        AndroidEnvironment.UnhandledExceptionRaiser -= OnUnhandledException;
-        base.OnDestroy();
-    }
-
-    private void OnUnhandledException(object? sender, RaiseThrowableEventArgs e)
-    {
-        // Diagnostic build: keep the process alive long enough to show the actual
-        // managed exception instead of silently dropping back to the launcher.
-        e.Handled = true;
-        RunOnUiThread(() => ShowFatalError(e.Exception));
-    }
-
-    private void ShowFatalError(Exception exception)
-    {
-        if (_showingFatalError) return;
-        _showingFatalError = true;
-
-        try
-        {
-            var details = exception.ToString();
-
-            new AlertDialog.Builder(this)
-                .SetTitle("EAVdrop Android error")
-                .SetMessage(details)
-                .SetPositiveButton("Close", (_, _) => FinishAndRemoveTask())
-                .SetCancelable(false)
-                .Show();
-        }
-        catch
-        {
-            FinishAndRemoveTask();
-        }
-    }
 }
