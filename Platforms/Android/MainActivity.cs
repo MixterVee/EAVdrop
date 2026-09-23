46ef30f6227c5b2b33a9259c6c1ac0069f235c85
MixterVee <mixtervandal@gmail.com>
2026-09-23 12:00:24 -0400

    Show Android startup exception details

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,6 +1,8 @@
 using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
 
 namespace EAVdrop;
 
@@ -15,4 +17,68 @@ namespace EAVdrop;
     Categories = new[] { Intent.CategoryLeanbackLauncher })]
 public class MainActivity : MauiAppCompatActivity
 {
+    private bool _showingFatalError;
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        AndroidEnvironment.UnhandledExceptionRaiser += OnUnhandledException;
+
+        try
+        {
+            base.OnCreate(savedInstanceState);
+        }
+        catch (Exception ex)
+        {
+            ShowFatalError(ex);
+        }
+    }
+
+    protected override void OnDestroy()
+    {
+        AndroidEnvironment.UnhandledExceptionRaiser -= OnUnhandledException;
+        base.OnDestroy();
+    }
+
+    private void OnUnhandledException(object? sender, RaiseThrowableEventArgs e)
+    {
+        // Diagnostic build: keep the process alive long enough to show the actual
+        // managed exception instead of silently dropping back to the launcher.
+        e.Handled = true;
+        RunOnUiThread(() => ShowFatalError(e.Exception));
+    }
+
+    private void ShowFatalError(Exception exception)
+    {
+        if (_showingFatalError) return;
+        _showingFatalError = true;
+
+        try
+        {
+            var details = exception.ToString();
+
+            new AlertDialog.Builder(this)
+                .SetTitle("EAVdrop Android error")
+                .SetMessage(details)
+                .SetPositiveButton("Copy", (_, _) =>
+                {
+                    try
+                    {
+                        var clipboard = (ClipboardManager?)GetSystemService(ClipboardService);
+                        clipboard?.SetPrimaryClip(ClipData.NewPlainText("EAVdrop error", details));
+                    }
+                    catch
+                    {
+                    }
+
+                    _showingFatalError = false;
+                })
+                .SetNegativeButton("Close", (_, _) => FinishAndRemoveTask())
+                .SetCancelable(false)
+                .Show();
+        }
+        catch
+        {
+            FinishAndRemoveTask();
+        }
+    }
 }
